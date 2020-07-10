326fd62f7b3b9867516847c2e52772acb30698ac
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2020-07-10 13:21:15 -0300

    Add code coverage. Run rubocop

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Event do
-  subject(:event) { Event.new(name: 'the name') }
+  subject(:event) { described_class.new(name: 'the name') }
 
   it 'has a name' do
     expect(event.name).to eq('the name')
