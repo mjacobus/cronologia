daf38b91668a37af0397d4608388725528bce490
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2020-07-10 11:25:15 -0300

    Disable unwanted generators

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,15 @@ module Cronologia
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.i18n.available_locales = ["pt-BR"]
+    config.i18n.default_locale = 'pt-BR'
+    config.time_zone = 'America/Sao_Paulo'
+
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+    end
   end
 end
