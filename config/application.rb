a3ba9df7e17cb78f35bbae4d3ff1f1e6c5243e78
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2020-07-10 11:41:47 -0300

    Disable database and stuff that don't matter

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,29 @@
 require_relative 'boot'
 
-require 'rails/all'
+# https://github.com/rails/rails/blob/master/railties/lib/rails/all.rb
+# require 'rails/all'
+# rubocop:disable Style/RedundantBegin
+
+require "rails"
+
+[
+  # 'active_record/railtie',
+  # 'active_storage/engine',
+  'action_controller/railtie',
+  'action_view/railtie',
+  # 'action_mailer/railtie',
+  # 'active_job/railtie',
+  # 'action_cable/engine',
+  # 'action_mailbox/engine',
+  # 'action_text/engine',
+  # 'rails/test_unit/railtie',
+  'sprockets/railtie',
+].each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
