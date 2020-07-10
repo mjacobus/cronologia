94a0d05521dba7f141a15918ae2d046efafa3ed9
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2020-07-10 11:30:38 -0300

    Rubocop files

--- test/controllers/chronology_controller_test.rb
+++ test/controllers/chronology_controller_test.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class ChronologyControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get chronology_index_url
     assert_response :success
   end
-
 end
