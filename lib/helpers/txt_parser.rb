c0b13b96b87006fe39a1f65aeb5c81519097c0e0
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2020-07-10 23:14:25 -0300

    Add timeline parser

---
+++ lib/helpers/txt_parser.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+module Helpers
+  class TxtParser
+    def parse(text)
+      text.split("\n").map do |line|
+        parse_line(line)
+      end
+    end
+
+    private
+
+    def parse_line(line)
+      {
+        name: name(line),
+        year: year(line),
+        month: month(line),
+        day: day(line),
+        precision: precision(line)
+      }.compact
+    end
+
+    def name(line)
+      line.split('EC').last.strip
+    end
+
+    def year(line)
+      y = line.match(/\d+/).to_a.first.to_i
+
+      if line.split(y.to_s).last.strip.split(' ').first.casecmp('aec').zero?
+        y *= -1
+      end
+
+      y
+    end
+
+    def month(_line)
+      nil
+    end
+
+    def day(_line)
+      nil
+    end
+
+    def precision(line)
+      line = line.downcase
+
+      if line.starts_with?('c.')
+        return 'about'
+      end
+
+      if line.starts_with?('d.')
+        return 'after'
+      end
+
+      if line.starts_with?('a.')
+        return 'before'
+      end
+
+      'precise'
+    end
+  end
+end
